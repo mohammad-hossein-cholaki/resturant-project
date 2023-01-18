2e0950accdee8212d1ad4dff321af8afb3859085
hossein-cholaki <110971150+hossein-cholaki@users.noreply.github.com>
2023-01-18 16:55:31 +0330

    make a slider with a template of swiperjs.com

--- home.js
+++ home.js
@@ -13,5 +13,26 @@ menu.onclick =()=>{
 
 
 
+    // Initialize Swiper 
+    // <script>
+      var swiper = new Swiper(".home-slider", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 7500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        // navigation: {
+        //   nextEl: ".swiper-button-next",
+        //   prevEl: ".swiper-button-prev",
+        // },
+        loop:true
+      });
+    /* </script> */
+
 // end of slider code
 
