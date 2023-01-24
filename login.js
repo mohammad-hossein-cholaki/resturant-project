71e691936fed59c6be22ffc3e65633c6c4928cae
hossein-cholaki <110971150+hossein-cholaki@users.noreply.github.com>
2023-01-24 17:51:53 +0330

    finish login page

--- login.js
+++ login.js
@@ -1,17 +1,13 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '416351d1b2mshb109934ca946399p1c0a87jsn5cab645fbe7f',
-// 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-// 	}
-// };
 
-// fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
-// 	.then(response => response.json())
-// 	.then(response => {
-//     console.log(response)
-//     console.log(response.results)
-//     console.log(response.results[0])
-//     console.log(response.results[0].thumbnail_url)
-//   })
-// 	.catch(err => console.error(err));
+const submit=document.getElementById("submit");
+
+submit.onclick=()=>{
+    const username=document.getElementById("username").value;
+    const password=document.getElementById("password").value;
+
+    if(username==="admin" && password==="admin"){
+        alert('welcome');
+    }else{
+        alert('invalid user name or password')
+    }
+}
